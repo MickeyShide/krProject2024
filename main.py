b53481d7d2419cd951838f38443aea944a7fece0
MickeyShide <mickey_shide@list.ru>
2024-09-01 20:14:11 +0300

    123

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import asyncio
-import logging
-import sys
 
-import db.models, db.requests
-from aiogram import Bot, Dispatcher
-from config import TOKEN, LOGS_FILENAME
-from handlers import router
-from db.models import async_main
-bot = Bot(token=TOKEN)
+import dp as dp
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command
+from aiogram.types import Message
+
+TOKEN = '6476560422:AAH054CouMc0gItojp0vCYctmXkp-10T1z8'
+
+
 async def main():
-    #print("\n\n\n\n\n\n\n\n\n\n")
-    #for user in await db.requests.get_users():
-    #    user: db.models.Users = user
-    #    #print("\n\n\n", user.messages)
-    #    for message in user.messages:
-    #        message: db.models.Messages = message
-    #        print("\n\n\n" + message.text)
-    await async_main()
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
-    dp.include_router(router)
+    assert isinstance(bot)
     await dp.start_polling(bot)
+@dp.message(Command("start"))
+async def begin(message: types.Message):
+    await message.answer("Пришли фотку лох")
+@dp.message(F.animation)
+async def echo_gif(message: Message):
+    await message.reply_animation(message.animation.file_id)
+
 
 if __name__ == "__main__":
-    #logging.basicConfig(filename=LOGS_FILENAME, filemode="a", level=logging.INFO)
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Exit")
+    asyncio.run(main())
